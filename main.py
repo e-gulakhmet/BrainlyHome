b9c0eb3fc56bd8defe1f3820bb3003ccd7a41304
WhoMan <enes515254@gmail.com>
2020-07-13 00:21:55 +0600

    Add logging response

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import logging
+import time
 
 import mqtt
 
 
 
 def main():
-    logging.basicConfig(filename="brainlyhome.log", level=logging.DEBUG, format="%(asctime)s [%(levelname)s] %(name)s : %(message)s")
+    logging.basicConfig(level=logging.DEBUG, format="%(asctime)s [%(levelname)s] %(name)s : %(message)s")
 
     print("BrainlyHome")
     client = mqtt.Mqtt("192.168.1.112", "base")
-
     client.connect()
     client.publish("base/test", "hello world")
     client.disconnect()    
--- mqtt.py
+++ mqtt.py
@@ -1,5 +1,10 @@
 import paho.mqtt.client as paho_mqtt
 import logging
+import time
+
+
+
+# TODO: Добавить проверку подключения перед исполнением функции
 
 
 
@@ -7,23 +12,46 @@ class Mqtt():
     def __init__(self, broker_adress, client_id):
         self.broker_adress = broker_adress # Адресс брокера в сети
         self.client_id = client_id
+
         self.logger = logging.getLogger('CLIENT')
         # Инициализируем клиент
         self.client = paho_mqtt.Client(self.client_id)
-        self.logger.info("Initialized")
+
+        # Подключаемся к событиям клиента для отладки
+        self.client.on_connect = self.on_connect
+        self.client.on_disconnect = self.on_disconnect
+        self.client.on_publish = self.on_publish
+
+
     
     def connect(self):
         # Подключаемся к брокеру
+        self.logger.info("Connecting...")
         self.client.connect(self.broker_adress)
         self.client.loop_start()
+
+    def on_connect(self, client, userdata, flags, rc):
         self.logger.info("Connected")
+
+
 
     def disconnect(self):
         # Оключаемся от брокера
-        self.client.loop_stop()
-        self.client.disconnect()
+        if self.client.is_connected() is True:
+            self.logger.info("Disconnecting...")
+            self.client.loop_stop()
+            self.client.disconnect()
+
+    def on_disconnect(self, client, userdata, rc):
         self.logger.info("Disconnected")
+
     
+
     def publish(self, topic, message):
         # Отправляем сообщение
-        self.client.publish(topic, message)
+        if self.client.is_connected():
+            self.logger.info("Publishing{Topic: " + topic + ", Message: " + message + "}...")
+            self.client.publish(topic, message)
+
+    def on_publish(self, client, userdata, mid):
+        self.logger.info("Published")
